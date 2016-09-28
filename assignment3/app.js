001be28b596d944d0019fd358aa04705c845847f
riabchenko <riabchenko.vlad@gmail.com>
2016-09-28 23:42:51 +0200

    https bug

--- assignment3/app.js
+++ assignment3/app.js
@@ -5,7 +5,7 @@
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
     .directive('foundItems', foundItemsDirective)
-    .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
