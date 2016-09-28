56c46a68c8a896a3994045c0c189460c04b19d53
riabchenko <riabchenko.vlad@gmail.com>
2016-09-28 23:40:18 +0200

    assignment 3

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Coursera's Angular course assignments
 
 - [Assignment 1](https://vria.github.io/coursera-angular/assignment1/)
 - [Assignment 2](https://vria.github.io/coursera-angular/assignment2/)
+- [Assignment 3](https://vria.github.io/coursera-angular/assignment3/)
---
+++ assignment3/app.js
@@ -0,0 +1,59 @@
+(function(){
+    "use strict";
+    
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', foundItemsDirective)
+    .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var narrowItDown = this;
+
+        narrowItDown.doNarrow = function() {
+            MenuSearchService.getMatchedMenuItems(narrowItDown.searchTerm)
+            .then(function(foundItems) {
+                narrowItDown.found = foundItems;
+            });
+        }
+
+        narrowItDown.removeItem = function(index) {
+            narrowItDown.found.splice(index, 1);
+        }
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+            return $http.get(ApiBasePath + '/menu_items.json')
+                .then(function(response) {
+                    return response.data.menu_items.filter(function(menuItem) {
+                        return menuItem.description.indexOf(searchTerm) >= 0;
+                    });
+                })
+                .catch(function (error) {
+                    console.error(error);
+                })
+        }
+    }
+
+    function foundItemsDirective() {
+        return {
+            restrict: 'A',
+            templateUrl: 'templates/foundItems.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            },
+            controller: function() {
+
+            },
+            controllerAs: 'foundItemsCtrl',
+            bindToController: true
+        };
+    }
+
+})();
